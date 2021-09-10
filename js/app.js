c81fc057c63a90a20b35a5ae3d322e697f48f133
Mahmoud Fahmy <mafahmypc@hotmail.com>
2021-09-11 00:36:43 +0200

    Added the set active class, added to the topButton

--- js/app.js
+++ js/app.js
@@ -19,82 +19,118 @@
 */
 const sections = document.querySelectorAll('[data-nav]');
 const uList = document.querySelector('ul');
+const windowHeight = visualViewport.height;
+const links = document.querySelectorAll('a');
+const lists = document.querySelectorAll('li');
+const toTheTop = document.querySelector('button');
 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+*/ 
 
-
+toTheTop.addEventListener('click', function()
+{
+    window.scrollTo(0,0);
+});
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ * The function to build the navigation once the node list [data-nav] updated
 */
-function navigationBuild()
-{
-    sections.forEach(function (element, index, nodes)
+
+function navigationBuild() {
+    sections.forEach(function (element, index ) 
     {
+        
         const list = document.createElement('li');
-        // list.innerHTML = '<a href="#section + nodes + 1">section 1</a>';
-        let indexPlus1 = index +1;
+        let indexPlus1 = index + 1;
         let a = document.createElement('a');
         let link = document.createTextNode("Section " + indexPlus1);
         a.appendChild(link);
-        a.href = "#section"+ indexPlus1;
-        // a.title = "Section " + indexPlus1;
+        a.href = "#section" + indexPlus1;
+        a.style.textDecoration = 'none';
+        a.style.color = 'black';
         list.appendChild(a);
-        uList.appendChild(list);
-    })    
-    
+        uList.appendChild(list);    
+    });
 }
-
 // build the nav
 // changing the text color of the whole nav menu
 
 navigationBuild();
 
-// const list = document.createElement('li');
-// ulist.setAttribute('style', '.menu__link');
-// uList.style.color = 'black';
-// list.textContent = 'Section 1';
-// uList.appendChild(list);
-// uList.innerHTML = '<li> <a href = "#section1"> Section 1 <a/> <li/> <li><a href = "#section2"> Section 2 <a/></li> <li> <a href = "#section3"> Section 3 </a></li>'
-// const sectionsTitle = ['#section1', '#section2', '#section3'];
-// for (let linkTitle = 0; linkTitle < sectionsTitle.length; linkTitle ++ )
-// uList.innerHTML = '<li> <a href = sectionsTitle[linkTitle] > Section   <a/> <li/> ';
-
-
-
-                   
-
-
-
-
-
-const section = document.getElementById('section3');
-
 // Add class 'active' to section when near top of viewport
-// function testing()
-// {
-    document.addEventListener('scroll', function() 
+
+
+document.addEventListener('scroll', function () 
+{
+    const firstSection = document.getElementById('section1');
+    const boundry1 = firstSection.getBoundingClientRect();
+    
+    if (boundry1.top + (boundry1.height / 1.4) <= windowHeight && (boundry1.top + (boundry1.height) / 3) >= 0) 
     {
-    const boundry = section.getBoundingClientRect();
-    const windowHeight = visualViewport.height;
-    if (boundry.top + (boundry.height/1.4) <= windowHeight && (boundry.top + (boundry.height)/3) >= 0 )
-    {
-       section.classList.add('your-active-class');
-    } 
-    else {
-        section.classList.remove('your-active-class');
+        firstSection.classList.add('your-active-class');
     }
-}); 
+    else 
+    {
+        firstSection.classList.remove('your-active-class');
+    }
+});
+
+document.addEventListener('scroll', function () 
+{
+    const secondSection = document.getElementById('section2');
+    const boundry2 = secondSection.getBoundingClientRect();
     
-// }
+    if (boundry2.top + (boundry2.height / 1.4) <= windowHeight && (boundry2.top + (boundry2.height) / 3) >= 0) 
+    {
+        secondSection.classList.add('your-active-class');
+    }
+    else 
+    {
+        secondSection.classList.remove('your-active-class');
+    }
+});
+
+
+document.addEventListener('scroll', function () 
+{
+    const thirdSection = document.getElementById('section3');
+    const boundry3 = thirdSection.getBoundingClientRect();
     
+    if (boundry3.top + (boundry3.height / 1.4) <= windowHeight && (boundry3.top + (boundry3.height) / 3) >= 0) 
+    {
+        thirdSection.classList.add('your-active-class');
+    }
+    else 
+    {
+        thirdSection.classList.remove('your-active-class');
+    }
+});
+
+document.addEventListener('scroll', function () 
+{
+    const forthSection = document.getElementById('section4');
+    const boundry4 = forthSection.getBoundingClientRect();
+    
+    if (boundry4.top + (boundry4.height / 1.4) <= windowHeight && (boundry4.top + (boundry4.height) / 3) >= 0) 
+    {
+        forthSection.classList.add('your-active-class');
+       
+    }    
+    else 
+    {
+        forthSection.classList.remove('your-active-class');
+        
+    }
+});
+
+
+
 
 // Scroll to anchor ID using scrollTO event
 
@@ -102,7 +138,7 @@ const section = document.getElementById('section3');
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
