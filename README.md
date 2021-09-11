97ee9aaf97aef09df1622a586915f8e3b3323b5f
Mahmoud Fahmy <mafahmypc@hotmail.com>
2021-09-11 22:01:14 +0200

    Writing README and comments

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+Landing page project is my first project, I ran into some chalanges like building the navigation and make it update once more sections is added, figuring out the best way to loop the node list, creating the <a> anchor, getting rid of the red color once the link is clicked, changing the style of it, setting the class of the section to active, and the scroll smoothly, and while I am applying the scrollTO behavior : 'smooth' I made the set class to active the right way.
+
+Features:
+- once the nodelist updated , it is in the nav bar with all the functionality 
--- js/app.js
+++ js/app.js
@@ -42,28 +42,27 @@ toTheTop.addEventListener('click', function()
  * Begin Main Functions
  * The function to build the navigation once the node list [data-nav] updated
 */
+// looping the sections list
 function navigationBuild() {
     sections.forEach(function (element, index ) 
     {
-        const boundry = element.getBoundingClientRect();
+        const boundry = element.getBoundingClientRect();                 //getting the boundry details of each section
         const list = document.createElement('li');
-        let indexPlus1 = index + 1;
-        let a = document.createElement('a');
-        let link = document.createTextNode("Section " + indexPlus1);
-        
-        a.appendChild(link);
-        a.href = "#section" + indexPlus1;
-        a.style.textDecoration = 'none';
-        a.style.color = 'black';
-        a.addEventListener('click',smooth);
+        let indexPlus1 = index + 1;                                      // adding 1 to the index starting with zero                   
+        let a = document.createElement('a');                             // creating the anchor element           
+        let link = document.createTextNode("Section " + indexPlus1);     // applying the section data-nav 
+        a.appendChild(link);                                             // linking the data-nav to the anchor
+        a.href = "#section" + indexPlus1;                                // setting the anchor href
+        a.style.textDecoration = 'none';                                 // getting rid of the underline
+        a.style.color = 'black';                                         // setting the style to black
+        a.addEventListener('click',smooth);                              // adding a click event to fire the smooth function
+        list.appendChild(a);                                             // adding the anchor to the list
+        uList.appendChild(list);                                         // adding the list to the unordered list
 
-        list.appendChild(a);
-        uList.appendChild(list); 
-
-        function smooth(e)
+        function smooth(e)                                               // function to sctroll smoothly
         {
-            e.preventDefault();
-            const href = this.getAttribute('href');
+            e.preventDefault();                                          // prevent the link default action
+            const href = this.getAttribute('href');                        
             const offsetTop = document.querySelector(href).offsetTop;
             scrollTo(
                 {
@@ -72,7 +71,7 @@ function navigationBuild() {
                 }
             )
         };
-        document.addEventListener('scroll',function()
+        document.addEventListener('scroll',function()                    //scroll event to set and unset the class to active
         {
             const boundry = element.getBoundingClientRect();
             if (boundry.top + (boundry.height / 1.4) <= windowHeight && boundry.top + (boundry.height / 3) >=0)
