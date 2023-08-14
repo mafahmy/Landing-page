8305d56a3a0eeb7f9b02452be5e4a112f47164dd
Mahmoud fahmy <mafahmypc@hotmail.com>
2023-08-14 13:21:06 +0300

    updating app.js

--- js/app.js
+++ js/app.js
@@ -12,74 +12,87 @@
  * JS Standard: ESlint
  * 
 */
+
 const sections = document.querySelectorAll('[data-nav]');                // getting the all sections in one variable
-const uList = document.querySelector('ul');                              // selecting the unordered list
+const navigationList = document.querySelector('ul');                     // selecting the unordered list
 const windowHeight = visualViewport.height;                              // getting the view height
-const toTheTop = document.querySelector('button');                       // selecting the button 
+const toTheTop = document.querySelector('button');                       // selecting the button
+
 /**
- * Define Global Variables
- * 
-*//**
- * End Helper Functions
- * Begin Main Functions
- * The function to build the navigation once the node list [data-nav] updated
-*/
-// looping the sections list
+ * Creates an anchor element for the section with the given index.
+ * @param {number} index - The index of the section.
+ * @returns {HTMLAnchorElement} The created anchor element.
+ */
+function createSectionLink(index) {
+    // Create the anchor element
+    const sectionLink = document.createElement('a');
+    // Create the link text
+    const linkText = document.createTextNode(`Section ${index + 1}`);
+    // Append the link text to the anchor element
+    sectionLink.appendChild(linkText);
+    // Set the href attribute of the anchor element
+    sectionLink.href = `#section${index + 1}`;
+    // Add a class to the anchor element
+    sectionLink.classList.add('section-link');
+    // Add a click event listener to the anchor element
+    sectionLink.addEventListener('click', smoothScroll);
+    // Return the created anchor element
+    return sectionLink;
+}
+
+/**
+ * Smoothly scrolls to the target of the clicked anchor element.
+ * @param {Event} e - The click event.
+ */
+function smoothScroll(e) {
+    // Prevent the default action of the click event
+    e.preventDefault();
+    // Get the href attribute of the clicked anchor element
+    const href = this.getAttribute('href');
+    // Get the offsetTop of the target element
+    const offsetTop = document.querySelector(href).offsetTop;
+    // Scroll to the target element with a smooth behavior
+    scrollTo({
+        behavior: 'smooth',
+        top: offsetTop
+    });
+}
+/**
+ * A function to build the navigation based on the sections in the HTML
+ */
 function navigationBuild() {
-    sections.forEach(function (element, index ) 
-    {
-        const boundry = element.getBoundingClientRect();                 //getting the boundry details of each section
-        const list = document.createElement('li');
-        let indexPlus1 = index + 1;                                      // adding 1 to the index starting with zero                   
-        let a = document.createElement('a');                             // creating the anchor element           
-        let link = document.createTextNode("Section " + indexPlus1);     // applying the section data-nav 
-        a.appendChild(link);                                             // linking the data-nav to the anchor
-        a.href = "#section" + indexPlus1;                                // setting the anchor href
-        a.style.textDecoration = 'none';                                 // getting rid of the underline
-        a.style.color = 'black';                                         // setting the style to black
-        a.addEventListener('click',smooth);                              // adding a click event to fire the smooth function
-        list.appendChild(a);                                             // adding the anchor to the list
-        uList.appendChild(list);                                         // adding the list to the unordered list
-
-        function smooth(e)                                               // function to sctroll smoothly
-        {
-            e.preventDefault();                                          // prevent the link default action
-            const href = this.getAttribute('href');                        
-            const offsetTop = document.querySelector(href).offsetTop;
-            scrollTo(
-                {
-                    behavior: 'smooth',
-                    top : offsetTop
-                }
-            )
-        };   
-    });   
-}    
-// build the nav
-
+    sections.forEach((element, index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-nav', element.getAttribute('data-nav'));
+        const sectionLink = createSectionLink(index);
+        listItem.appendChild(sectionLink);
+        navigationList.appendChild(listItem);
+    })
+}
 navigationBuild();
 
-document.addEventListener('scroll',function()                    //scroll event to set and unset the class to active
+document.addEventListener('scroll', function ()                    //scroll event to set and unset the class to active
 {
-    sections.forEach(function (element)
-    {
+    sections.forEach(function (element) {
         const boundry = element.getBoundingClientRect();
-        if (boundry.top + (boundry.height / 1.4) <= windowHeight && boundry.top + (boundry.height / 3) >=0)
-        {
+        const listItem = document.querySelector(`li[data-nav = "${element.getAttribute('data-nav')}"]`);
+        if (boundry.top + (boundry.height / 1.4) <= windowHeight && boundry.top + (boundry.height / 3) >= 0) {
             element.classList.add('your-active-class');
+            listItem.classList.add('active');
         }
         else {
             element.classList.remove('your-active-class');
+            listItem.classList.remove('active');
         }
-   });
+    });
 });
-toTheTop.addEventListener('click', function()                            // adding a click event to scroll to the top button
-    {
+toTheTop.addEventListener('click', function ()                         // adding a click event to scroll to the top button
+{
     scrollTo(
         {
             top: 0,
             behavior: 'smooth'
         }
-        );
-     });
+    );
+});
 
